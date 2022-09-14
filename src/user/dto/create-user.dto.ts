ab5052ea327889fd15afdd327f3ee34e6d82c2fa
Arzin <a.nikolyan@digitalpome.com>
2022-09-14 12:00:31 +0400

    Added create user validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,35 @@
 import { IsNotEmpty, IsEmail, MinLength, MaxLength } from "@nestjs/class-validator";
+import { IsPhoneNumber } from "class-validator";
+import { Unique } from "typeorm";
 
 export class CreateUserDto {
+    @IsNotEmpty()
+    @Unique('username-uniqe', ['username'])
+    @MinLength(2)
+    @MaxLength(20)
+    username: string
+    
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
+    first_name:string
+
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
+    last_name:string
+
     @IsNotEmpty()
     @IsEmail()
     email:string
 
     @IsNotEmpty()
-    @MinLength(2)
+    // @IsPhoneNumber('AM')
+    @IsPhoneNumber()
+    phone:string
+
+    @IsNotEmpty()
+    @MinLength(6)
     @MaxLength(20)
-    name:string
+    password:string
 }
