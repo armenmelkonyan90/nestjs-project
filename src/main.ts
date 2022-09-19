b47aa87c7f3ead7160281466e1406d84ec7dbb05
Arzin <a.nikolyan@digitalpome.com>
2022-09-19 11:50:22 +0400

    Make custom validation messages structure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { STATUS_CODES } from 'http';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors) => {
+      if (errors.length) {
+        let messages = [];
+       
+        for (let item of errors) {
+          let single = {};
+          let constraints = Object.values(item.constraints)
+          single[item.property] = constraints;
+          messages.push(single);
+        }
+
+        throw new BadRequestException({
+          error: "Unprocessible entity",
+          messages,
+          code: 422
+        })
+      }
+    },
+  }));
   await app.listen(3000);
 }
 bootstrap();
